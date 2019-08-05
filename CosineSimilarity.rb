29b0f47165cec82c07b06becb63e3227ef1ae4aa
川島　武士 <kawashima38@gmail.com>
2019-08-05 15:57:09 +0900

    add CosineSimilarity.rb

---
+++ CosineSimilarity.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+require 'matrix'
+
+module CosineSimilarity
+
+  module_function
+
+  def combination_calc(group)
+    # 集団のデータをベクトルに変換し、正規化
+    normalized_vectors = {}
+    group.each do |gk, gv|
+      normalized_vectors[gk] = Vector.elements(gv).normalize
+    end
+
+    # 組み合わせごとのコサイン類似度を出力
+    normalized_vectors.keys.combination(2) do |v1k, v2k|
+      puts "#{v1k} - #{v2k} = #{normalized_vectors[v1k].inner_product(normalized_vectors[v2k])}"
+    end
+  end
+end
+
+if $0 == __FILE__
+
+  writers = {
+    :kotaro_isaka => [3, 2, 1, 1],
+    :keigo_higashino => [2, 2, 1, 0],
+    :haruki_murakami => [1, 1, 2, 1],
+    :j_k_rowling => [1, 0, 1, 3]
+  }
+
+  CosineSimilarity.combination_calc(writers)
+
+end
